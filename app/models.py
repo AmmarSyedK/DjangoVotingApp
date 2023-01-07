ab45b66a2d93876a2b58c7b7baae910eff76b0f4
AmmarSyedK <ammarsyedk@gmail.com>
2023-01-07 21:57:09 +0100

    Voting application limited to 3 questions

--- app/models.py
+++ app/models.py
@@ -8,4 +8,7 @@ class Poll(models.Model):
     option_three = models.CharField(max_length=30)
     option_one_count = models.IntegerField(default=0)
     option_two_count = models.IntegerField(default=0)
-    option_two_count = models.IntegerField(default=0)
+    option_three_count = models.IntegerField(default=0)
+
+    def total(self):
+        return self.option_one_count + self.option_two_count + self.option_three_count
--- app/views.py
+++ app/views.py
@@ -1,18 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .forms import CreateForm
+from .models import Poll
 
 # Create your views here.
 def index(request):
-    context = {}
+    polls = Poll.objects.all()
+    context = {
+        'polls' : polls
+    }
     return render(request, 'app/index.html', context)
 
 def create(request):
-    context = {}
+    if request.method == 'POST':
+        form = CreateForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = CreateForm()
+    context = {
+        'form' : form
+    }
     return render(request, 'app/create.html', context)
 
 def vote(request, poll_id):
-    context = {}
+    poll = Poll.objects.get(pk=poll_id)
+    if request.method == 'POST':
+        selected_option = request.POST['poll']
+        if selected_option == 'option1':
+            poll.option_one_count += 1
+        elif selected_option == 'option2':
+            poll.option_two_count += 1
+        elif selected_option == 'option3':
+            poll.option_three_count += 1
+        else:
+            return HttpResponse(400, 'Invalid form')    
+        
+        poll.save()
+
+        return redirect('results', poll.id)
+
+    context = {
+        'poll' : poll
+    }
     return render(request, 'app/vote.html', context)
 
 def results(request, poll_id):
-    context = {}
+    poll = Poll.objects.get(pk=poll_id)
+    context = {
+        'poll' : poll
+    }
     return render(request, 'app/results.html', context)
